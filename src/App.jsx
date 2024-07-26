969326d774793ca39af3b5e2a4456edf54a93191
raaxidaa <rahideisgenderova67@gmail.com>
2024-07-26 12:29:51 +0400

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import Startups from './Pages/Startups';
@@ -9,6 +8,7 @@ import SeekFunding from './Pages/SeekFunding';
 import Investor from './Pages/Investor';
 import './App.css';
 import Footer from './Components/Footer';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
